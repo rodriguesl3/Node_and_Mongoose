02c335ac2d004c3bd45f7f1b05d608e7c4313a31
rodriguesl3 <l.rsouza@outlook.com>
2016-07-31 22:30:38 +0100

    refactoring folders

--- express/node-sample/app.js
+++ express/node-sample/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api/',usersAPI);
+app.use('/api/users',usersAPI);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- express/node-sample/modules/users/routes.js
+++ express/node-sample/modules/users/routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users',(req,res)=>{
+router.get('/',(req,res)=>{
     res.render('index',{title: 'Lucas Rodrigues'});
 });
+router.get('/json',(req,res)=>{
+    res.json({title: 'Lucas Rodrigues'});
+});
+
+
 module.exports = router;
