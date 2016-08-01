da4ab8756c63c3878e05c174296ce1dfa718598b
rodriguesl3 <l.rsouza@outlook.com>
2016-08-01 16:42:30 +0100

    refactoring dependencies

--- express/node-sample/modules/users/index.js
+++ express/node-sample/modules/users/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const router = require('./routerExpress');
-const Routes = require('./routes');
+const actions = require('./action')
+const Routes = require('./routes')(actions);
 const Router = require('./routesExpress')(Routes, router);
 
 module.exports = Router;
--- express/node-sample/modules/users/routes.js
+++ express/node-sample/modules/users/routes.js
@@ -1,33 +1,30 @@
 'use strict';
-const Actions = require('./action'); 
-
-var Routes = [
-    {
-        method:'post',
-        path:'/pokemon',
-        action: Actions.cadastrar
-    },
-    {
-        method:'get',
-        path:'/pokemon',
-        action:Actions.listar
-    },
-    {
-        method:'get',
-        path:'/pokemon/:id',
-        action:Actions.consultar
-    },
-    {
-        method:'put',
-        path:'/pokemon/:id',
-        action:Actions.alterar
-    },
-    {
-        method:'delete',
-        path: '/pokemon/:id',
-        action: Actions.remover
-    }
-]
-
-
-module.exports = Routes;
+module.exports =(Actions)=>{
+    return [
+        {
+            method:'post',
+            path:'/pokemon',
+            action: Actions.cadastrar
+        },
+        {
+            method:'get',
+            path:'/pokemon',
+            action:Actions.listar
+        },
+        {
+            method:'get',
+            path:'/pokemon/:id',
+            action:Actions.consultar
+        },
+        {
+            method:'put',
+            path:'/pokemon/:id',
+            action:Actions.alterar
+        },
+        {
+            method:'delete',
+            path: '/pokemon/:id',
+            action: Actions.remover
+        }
+    ];
+};
