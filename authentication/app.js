e521eba94202c557beeda24cdff4e202f7d22965
rodriguesl3 <l.rsouza@outlook.com>
2016-08-01 20:30:33 +0100

    creating first apis

--- authentication/app.js
+++ authentication/app.js
@@ -1,18 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const sessions = require('client-sessions');
+const csurf = require('csurf');
 
-var app = express();
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+const session = sessions({
+  cookieName      : "authentication",  
+  secret          : "LuC@s$!", //it should be a strong key
+  duration        : 7*60*60*1000,
+  activeDuration  : 5 * 60 * 1000,
+  httpOnly        : true, //web browser never has access my cookies secure 
+  ephemeral       : true //remve cookie when web browser close. 
+});
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,6 +33,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session);
+app.use(middleware.session);
 
 app.use('/', routes);
 app.use('/users', users);
